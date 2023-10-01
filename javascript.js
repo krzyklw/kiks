768182c55d7dde11787c3710f14986a6c7e07ce9
KrazyKlaw <78502394+krzyklw@users.noreply.github.com>
2023-10-01 11:27:58 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,24 @@
+    function updateTimer() {
+        const startDate = new Date(document.getElementById('startDate').textContent);
+        const now = new Date();
+
+        const diff = now - startDate;
+        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+        document.getElementById('months').innerText = '0';
+        document.getElementById('days').innerText = days;
+        document.getElementById('hours').innerText = hours;
+        document.getElementById('minutes').innerText = minutes;
+        document.getElementById('seconds').innerText = seconds;
+    }
+
+    setInterval(updateTimer, 1000);
+    updateTimer();
+
+    function togglePopup() {
+        var popup = document.getElementById('popup');
+        popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';
+    }
