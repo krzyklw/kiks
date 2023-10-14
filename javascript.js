f347e3e24b55967b30bbad271a2afae92f808092
KrazyKlaw <78502394+krzyklw@users.noreply.github.com>
2023-10-14 22:25:48 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,24 +1,25 @@
-    function updateTimer() {
-        const startDate = new Date(document.getElementById('startDate').textContent);
-        const now = new Date();
+function updateTimer() {
+    const startDate = new Date(document.getElementById('startDate').textContent);
+    const now = new Date();
 
-        const diff = now - startDate;
-        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-        document.getElementById('months').innerText = months;
-        document.getElementById('days').innerText = days;
-        document.getElementById('hours').innerText = hours;
-        document.getElementById('minutes').innerText = minutes;
-        document.getElementById('seconds').innerText = seconds;
+    let months = 0;
+    let days = Math.floor((now - startDate) / (1000 * 60 * 60 * 24));
+    
+    while (days >= 30) {
+        days -= 30;
+        months++;
     }
 
-    setInterval(updateTimer, 1000);
-    updateTimer();
+    const hours = Math.floor((now - startDate) / (1000 * 60 * 60)) % 24;
+    const minutes = Math.floor((now - startDate) / (1000 * 60)) % 60;
+    const seconds = Math.floor((now - startDate) / 1000) % 60;
 
-    function togglePopup() {
-        var popup = document.getElementById('popup');
-        popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';
-    }
+    document.getElementById('months').innerText = months;
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+}
+
+setInterval(updateTimer, 1000);
+updateTimer();
